d5d058dd6429cd0c028868a793cebec9335cbee6
Victory Audu <vinvic4life@gitlab.com>
2024-01-23 08:53:00 +0100

    Fix
    Log and COnfig FIx

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # SPDX-FileCopyrightText: 2021 grommunio GmbH
 # Interactive grommunio setup
-exec /usr/lib/systemd/systemd --log-level=debug --system --unit=multi-user.target
 
 DATADIR="${0%/*}"
 if [ "${DATADIR}" = "$0" ]; then
